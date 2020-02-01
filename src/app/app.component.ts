d0780995d32745068172ded608a58b40e1c03c82
daniel.gomez <daniel.gomez@techedgegroup.com>
2020-02-01 19:42:50 +0100

    Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'POALOF';
+  arraySongs = ['fille_en_feu', 'summer'];
+  playSong = '';
+
+  ngOnInit(): void {
+    const song = this.arraySongs[
+      Math.floor(Math.random() * this.arraySongs.length)
+    ];
+    this.playSong = `../assets/audio/${song}.mp3`;
+  }
 }
